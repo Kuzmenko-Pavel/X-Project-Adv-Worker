32d0b58185409fb8f1d8147d1b19997f0bcf582e
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2017-06-30 18:47:10 +0300

    adv logo style

--- x_project_adv_worker/db.py
+++ x_project_adv_worker/db.py
@@ -1,4 +1,5 @@
 import asyncpg
+from datetime import datetime
 
 
 async def init_db(app):
@@ -15,109 +16,142 @@ class Query(object):
     async def get_block(pool, block_src):
         async with pool.acquire() as connection:
             async with connection.transaction():
-                stmt = await connection.prepare('''SELECT * FROM public.mv_informer where guid=$1 LIMIT 1 OFFSET 0;''')
-                result = await stmt.fetchrow(block_src)
+                q = '''SELECT * FROM public.mv_informer where guid='%(guid)s' LIMIT 1 OFFSET 0;''' % {'guid': block_src}
+                stmt = await connection.prepare(q)
+                result = await stmt.fetchrow()
                 if result:
                     return dict(result)
             return None
 
     @staticmethod
-    async def get_campaigns(pool, block_id, block_domain, block_account):
+    async def get_campaigns(pool, block_id, block_domain, block_account, country, city, device, gender, cost):
+        gender_list = set('0')
+        cost_list = set('0')
+        gender_list.add(str(gender))
+        cost_list.add(str(cost))
+        date = datetime.now()
+        d = date.weekday() + 1
+        h = date.hour
+        m = date.minute
         async with pool.acquire() as connection:
             async with connection.transaction():
-                stmt = await connection.prepare('''SELECT ca.id,
-                                                       ca.guid,
-                                                       ca.title,
-                                                       ca.social,
-                                                       ca.brending,
-                                                       ca.recomendet_type,
-                                                       ca.recomendet_count,
-                                                       ca.account,
-                                                       ca.retargeting,
-                                                       ca.retargeting_type,
-                                                       ca.offer_by_campaign_unique,
-                                                       ca.unique_impression_lot,
-                                                       ca.html_notification
-                                                FROM campaign AS ca
-                                                INNER JOIN (
-                                                    SELECT  gt.id_cam as id  FROM geo AS gt
-                                                    INNER JOIN geo_lite_city AS gtl ON gt.id_geo=gtl.id WHERE (gtl.country='UA' AND (gtl.city='NOT FOUND' OR gtl.city='*')) OR (gtl.country='*' AND gtl.city='*')
-                                                    INTERSECT
-                                                    SELECT  dt.id_cam as id FROM campaign2device AS dt
-                                                    INNER JOIN device AS d ON dt.id_dev=d.id WHERE d.name='pc' OR d.name='**'
-                                                    INTERSECT
-                                                    SELECT ct.id FROM
-                                                            (SELECT cau.id FROM
-                                                                (
-                                                                SELECT c2c.id_cam AS id
-                                                                FROM campaign2categories AS c2c
-                                                                INNER JOIN categories2domain AS ct2d ON c2c.id_cat=ct2d.id_cat AND ct2d.id_dom=$2
-                                                                UNION
-                                                                SELECT c2da.id_cam AS id
-                                                                FROM campaign2domains AS c2da
-                                                                WHERE (c2da.id_dom=$2 OR c2da.id_dom=1) AND c2da.allowed=TRUE
-                                                                UNION
-                                                                SELECT c2aa.id_cam AS id
-                                                                FROM campaign2accounts AS c2aa
-                                                                WHERE (c2aa.id_acc=$3 OR c2aa.id_acc=1) AND c2aa.allowed=TRUE
-                                                                UNION
-                                                                SELECT c2ia.id_cam AS id
-                                                                FROM campaign2informer AS c2ia
-                                                                WHERE (c2ia.id_inf=$1 OR c2ia.id_inf=1) AND c2ia.allowed=TRUE
-                                                                ) AS cau
-                                                                EXCEPT
-                                                                SELECT caud.id
-                                                                FROM
-                                                                (
-                                                                SELECT c2dd.id_cam AS id
-                                                                FROM campaign2domains AS c2dd
-                                                                LEFT JOIN campaign2domains AS c2dde ON c2dd.id_cam=c2dde.id_cam AND c2dde.id_dom=$2 AND c2dde.allowed=TRUE
-                                                                WHERE c2dde.id_cam IS NULL AND ((c2dd.id_dom=$2 OR c2dd.id_dom=1) AND c2dd.allowed=FALSE)
-                                                                UNION
-                                                                SELECT c2ad.id_cam AS id
-                                                                FROM campaign2accounts AS c2ad
-                                                                LEFT JOIN campaign2accounts AS c2ade ON c2ad.id_cam=c2ade.id_cam AND c2ade.id_acc=$3 AND c2ade.allowed=TRUE
-                                                                WHERE c2ade.id_cam IS NULL AND ((c2ad.id_acc=$3 OR c2ad.id_acc=1) AND c2ad.allowed=FALSE)
-                                                                UNION
-                                                                SELECT c2id.id_cam AS id
-                                                                FROM campaign2informer AS c2id
-                                                                LEFT JOIN campaign2informer AS c2ide ON c2id.id_cam=c2ide.id_cam AND c2ide.id_inf=$1 AND c2ide.allowed=TRUE
-                                                                WHERE c2ide.id_cam IS NULL AND ((c2id.id_inf=$1 OR c2id.id_inf=1) AND c2id.allowed=FALSE)
-                                                                ) AS caud
-                                                            )AS ct
-                                                            INTERSECT
-                                                            SELECT crc.id AS id FROM
-                                                            (
-                                                                SELECT crcs.id_cam AS id FROM cron AS crcs
-                                                                    WHERE crcs.day = 1
-                                                                    AND
-                                                                        (
-                                                                            (
-                                                                                crcs.hour = 0 AND crcs.min <= 0 AND crcs.start_stop=TRUE
-                                                                            )
-                                                                            OR
-                                                                            (
-                                                                              crcs.hour < 0 AND crcs.start_stop=TRUE
-                                                                            )
-                                                                        )
-                                                                EXCEPT
-                                                                SELECT crcd.id_cam AS id FROM cron AS crcd
-                                                                    WHERE crcd.day = 1
-                                                                    AND
-                                                                        (
-                                                                            (
-                                                                                crcd.hour = 0 AND  crcd.min <= 0 AND crcd.start_stop=FALSE
-                                                                            )
-                                                                            OR
-                                                                            (
-                                                                              crcd.hour < 0 AND crcd.start_stop=FALSE
-                                                                            )
-                                                                        )
-                                                            ) AS crc
-                                                        ) AS c ON ca.id=c.id
-                                                        GROUP BY ca.id;
-                ''')
-                result = await stmt.fetch(block_id, block_domain, block_account)
+                q = '''
+    SELECT
+ca.*
+FROM mv_campaign AS ca
+  INNER JOIN (
+               SELECT gt.id_cam_pk AS id
+               FROM mv_geo AS gt
+                 INNER JOIN mv_geo_lite_city AS gtl ON gt.id_geo_pk = gtl.id
+               WHERE (gtl.country = '%(country)s' AND (gtl.city = '%(city)s' OR gtl.city = '*')) OR
+                     (gtl.country = '*' AND gtl.city = '*')
+                     
+               INTERSECT
+               SELECT dt.id_cam_pk AS id
+               FROM mv_campaign2device AS dt
+                 INNER JOIN mv_device AS d ON dt.id_dev_pk = d.id
+               WHERE d.name = '%(device)s' OR d.name = '**'
+
+               INTERSECT
+               SELECT t.id AS id
+               FROM mv_campaign AS t
+               WHERE t.gender in (%(gender)s) and t.cost in (%(cost)s)
+
+               INTERSECT
+               SELECT ct.id
+               FROM
+                 (SELECT cau.id
+                  FROM
+                    (
+                      SELECT c2c.id_cam_pk AS id
+                      FROM mv_campaign2categories AS c2c
+                        INNER JOIN mv_categories2domain AS ct2d ON c2c.id_cat_pk = ct2d.id_cat_pk AND ct2d.id_dom_pk = %(id_dom)d
+                      UNION
+                      SELECT c2da.id_cam AS id
+                      FROM mv_campaign2domains AS c2da
+                      WHERE (c2da.id_dom = %(id_dom)d OR c2da.id_dom = 1) AND c2da.allowed = TRUE
+                      UNION
+                      SELECT c2aa.id_cam AS id
+                      FROM mv_campaign2accounts AS c2aa
+                      WHERE (c2aa.id_acc = %(id_acc)d OR c2aa.id_acc = 1) AND c2aa.allowed = TRUE
+                      UNION
+                      SELECT c2ia.id_cam AS id
+                      FROM mv_campaign2informer AS c2ia
+                      WHERE (c2ia.id_inf = %(id_inf)d OR c2ia.id_inf = 1) AND c2ia.allowed = TRUE
+                    ) AS cau
+                  EXCEPT
+                  SELECT caud.id
+                  FROM
+                    (
+                      SELECT c2dd.id_cam AS id
+                      FROM mv_campaign2domains AS c2dd
+                        LEFT JOIN mv_campaign2domains AS c2dde
+                          ON c2dd.id_cam = c2dde.id_cam AND c2dde.id_dom = %(id_dom)d AND c2dde.allowed = TRUE
+                      WHERE c2dde.id_cam IS NULL AND ((c2dd.id_dom = %(id_dom)d OR c2dd.id_dom = 1) AND c2dd.allowed = FALSE)
+                      UNION
+                      SELECT c2ad.id_cam AS id
+                      FROM mv_campaign2accounts AS c2ad
+                        LEFT JOIN mv_campaign2accounts AS c2ade
+                          ON c2ad.id_cam = c2ade.id_cam AND c2ade.id_acc = %(id_acc)d AND c2ade.allowed = TRUE
+                      WHERE c2ade.id_cam IS NULL AND ((c2ad.id_acc = %(id_acc)d OR c2ad.id_acc = 1) AND c2ad.allowed = FALSE)
+                      UNION
+                      SELECT c2id.id_cam AS id
+                      FROM mv_campaign2informer AS c2id
+                        LEFT JOIN mv_campaign2informer AS c2ide
+                          ON c2id.id_cam = c2ide.id_cam AND c2ide.id_inf = %(id_inf)d AND c2ide.allowed = TRUE
+                      WHERE c2ide.id_cam IS NULL AND ((c2id.id_inf = %(id_inf)d OR c2id.id_inf = 1) AND c2id.allowed = FALSE)
+                    ) AS caud
+                 ) AS ct
+
+               INTERSECT
+               SELECT crc.id AS id
+               FROM
+                 (
+                   SELECT crcs.id_cam AS id
+                   FROM mv_cron AS crcs
+                   WHERE crcs.day = %(day)d
+                         AND
+                         (
+                           (
+                             crcs.hour = %(hour)d AND crcs.min <= %(min)d AND crcs.start_stop = TRUE
+                           )
+                           OR
+                           (
+                             crcs.hour < %(hour)d AND crcs.start_stop = TRUE
+                           )
+                         )
+                   EXCEPT
+                   SELECT crcd.id_cam AS id
+                   FROM mv_cron AS crcd
+                   WHERE crcd.day = %(day)d
+                         AND
+                         (
+                           (
+                             crcd.hour = %(hour)d AND crcd.min <= %(min)d AND crcd.start_stop = FALSE
+                           )
+                           OR
+                           (
+                             crcd.hour < %(hour)d AND crcd.start_stop = FALSE
+                           )
+                         )
+                 ) AS crc
+             ) AS c ON ca.id = c.id
+                  
+                ''' % {
+                    'country': country,
+                    'city': city,
+                    'device': device,
+                    'gender': ','.join(gender_list),
+                    'cost': ','.join(cost_list),
+                    'id_inf': block_id,
+                    'id_dom': block_domain,
+                    'id_acc': block_account,
+                    'day': d,
+                    'hour': h,
+                    'min': m
+                }
+                stmt = await connection.prepare(q)
+                result = await stmt.fetch()
                 return [dict(x) for x in result] if result else []
 
     @staticmethod
--- x_project_adv_worker/views/informer.py
+++ x_project_adv_worker/views/informer.py
@@ -7,11 +7,16 @@ from x_project_adv_worker.styler import Styler
 
 class InformerView(web.View):
     async def post(self):
-        result = dict({'place': [], 'social': [], 'dynamic_retargeting': [], 'account_retargeting':[]})
+        result = dict({'css': '', 'campaigns': [], 'block': {}})
         pool = self.request.app.pool
         data = await self.request.json()
         block_src = data.get('block_id', '')
         auto = data.get('auto', False)
+        country = data.get('country', 'NOT FOUND')
+        city = data.get('city', 'NOT FOUND')
+        device = data.get('device', 'pc')
+        cost = data.get('cost', 0)
+        gender = data.get('gender', 0)
         styler = Styler(data.get('w', 0), data.get('h', 0))
         block_result = await self.request.app.query.get_block(pool=pool, block_src=block_src)
         if block_result is None:
@@ -23,23 +28,32 @@ class InformerView(web.View):
         retargeting_branch = block_result.get('retargeting_branch', True)
         retargeting_account_branch = block_result.get('retargeting_branch', True)
         social_branch = block_result.get('account', True)
-        if not auto:
+        if not auto and not block_result.get('dynamic', False):
             styler.merge(ujson.loads(block_result.get('ad_style')))
 
         campaigns_result = await  self.request.app.query.get_campaigns(pool=pool, block_id=block_id,
                                                                        block_domain=block_domain,
-                                                                       block_account=block_account)
+                                                                       block_account=block_account,
+                                                                       country=country,
+                                                                       city=city,
+                                                                       device=device,
+                                                                       cost=cost,
+                                                                       gender=gender)
         for campaign in campaigns_result:
-            # styler.add(str(campaign['id']), 'Style_1')
+            if campaign['style_type'] not in ['default', 'Block', 'RetBlock', 'RecBlock']:
+                styler.add(str(campaign['id']), 'Style_1')
+
             if campaign['social'] and social_branch:
-                result['social'].append(campaign['id'])
+                result['campaigns'].append(campaign)
             elif not campaign['social'] and not campaign['retargeting'] and place_branch:
-                result['place'].append(campaign['id'])
-            elif not campaign['social'] and campaign['retargeting'] and campaign[
-                'retargeting_type'] == 'offer' and retargeting_branch:
-                result['dynamic_retargeting'].append(campaign['id'])
-            elif not campaign['social'] and campaign['retargeting'] and campaign[
-                'retargeting_type'] == 'account' and retargeting_account_branch:
-                result['account_retargeting'].append(campaign['id'])
+                result['campaigns'].append(campaign)
+            elif not campaign['social'] and campaign['retargeting'] and campaign['retargeting_type'] == 'offer' and retargeting_branch:
+                result['campaigns'].append(campaign)
+            elif not campaign['social'] and campaign['retargeting'] and campaign['retargeting_type'] == 'account' and retargeting_account_branch:
+                result['campaigns'].append(campaign)
         result['css'] = await styler()
+        result['block']['id'] = block_result.get('id', 0)
+        result['block']['guid'] = block_result.get('guid', '')
+        result['block']['headerHtml'] = block_result.get('headerHtml', '')
+        result['block']['footerHtml'] = block_result.get('footerHtml', '')
         return web.json_response(result, dumps=ujson.dumps)
