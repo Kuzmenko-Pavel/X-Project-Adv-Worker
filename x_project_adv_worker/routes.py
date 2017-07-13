eb8f74621c9da514ab99367d44bbd08af60fe81b
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2017-07-13 16:37:53 +0300

    change url

--- x_project_adv_worker/routes.py
+++ x_project_adv_worker/routes.py
@@ -4,14 +4,14 @@ from .views import (IframesView, InformerView,
 
 
 def setup_routes(app):
-    app.router.add_route('GET', '/pub', IframesView)
-    app.router.add_route('POST', '/pub', IframesView)
-    app.router.add_route('POST', '/informer.json', InformerView)
-    app.router.add_route('POST', '/place.json', OffersPlaceView)
-    app.router.add_route('POST', '/social.json', OffersSocialView)
-    app.router.add_route('POST', '/account_retargeting.json', OffersAccountRetargetingView)
-    app.router.add_route('POST', '/dynamic_retargeting.json', OffersDynamicRetargetingView)
+    app.router.add_route('GET', '/v1/pub', IframesView)
+    app.router.add_route('POST', '/v1/pub', IframesView)
+    app.router.add_route('POST', '/v1/informer.json', InformerView)
+    app.router.add_route('POST', '/v1/place.json', OffersPlaceView)
+    app.router.add_route('POST', '/v1/social.json', OffersSocialView)
+    app.router.add_route('POST', '/v1/account_retargeting.json', OffersAccountRetargetingView)
+    app.router.add_route('POST', '/v1/dynamic_retargeting.json', OffersDynamicRetargetingView)
 
-    app.router.add_route('GET', '/test', TestView)
+    app.router.add_route('GET', '/v1/test', TestView)
     app.router.add_route('GET', '/bl.js', Dummy)
     app.router.add_route('POST', '/bl.js', Dummy)
