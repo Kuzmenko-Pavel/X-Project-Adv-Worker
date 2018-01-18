9b904aeb160c8b3634c1a12836b0a626cacbdcfe
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-01-18 19:16:58 +0200

    delete debug view

--- x_project_adv_worker/routes.py
+++ x_project_adv_worker/routes.py
@@ -13,9 +13,3 @@ def setup_routes(app):
     app.router.add_route('POST', '/v1/dynamic_retargeting.json', OffersDynamicRetargetingView)
     app.router.add_static('/v1/static/', 'x_project_adv_worker/static/', append_version=True)
 
-    if app['config']['debug']['status']:
-        app.router.add_route('GET', '/v1/test', TestView)
-        app.router.add_route('GET', '/bl.js', Dummy)
-        app.router.add_route('POST', '/bl.js', Dummy)
-        app.router.add_route('GET', '/logger.json', Dummy)
-        app.router.add_route('POST', '/logger.json', Dummy)
