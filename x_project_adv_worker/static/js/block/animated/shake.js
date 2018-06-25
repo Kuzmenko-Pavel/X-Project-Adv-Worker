7576b82c4429235fa0a5c9db8b23869297ee9875
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-06-25 15:08:29 +0300

    optimisation

---
+++ x_project_adv_worker/static/js/block/animated/shake.js
@@ -0,0 +1,43 @@
+/**
+ * Created by kuzmenko-pavel on 14.04.17.
+ */
+define([
+    './../jquery',
+    './jquery.easing',
+    './jquery.transform2d'
+], function (jQuery) {
+    return function (elements) {
+        if (this.mouseInBlock) {
+            return;
+        }
+        if (!this.browser.isOldIE && this.device === 'pc') {
+            var interval, distance, time;
+            var an_f = function (el) {
+                jQuery(el).css({
+                    'transform': "",
+                    'filter': '',
+                    'rotate': '',
+                    'rotateY': ''
+                });
+            };
+            elements.each(function (
+                i,
+                el
+            ) {
+                interval = 50;
+                distance = 3;
+                time = 15;
+                for (var iter = 0; iter < (time + 1); iter++) {
+                    jQuery(el).animate(
+                        {
+                            transform: (iter % 2 === 0 ? 'rotate(' + distance + 'deg)' : 'rotate(-' + distance + 'deg)')
+                        },
+                        interval,
+                        'easeInOutBack',
+                        an_f(el)
+                    );
+                }
+            });
+        }
+    };
+});
