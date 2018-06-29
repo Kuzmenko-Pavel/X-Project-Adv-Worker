2215b85328aaabb29aca74678c01935ebce571d2
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-06-29 23:56:51 +0300

    fix recomendet style

--- x_project_adv_worker/routes.py
+++ x_project_adv_worker/routes.py
@@ -5,4 +5,4 @@ def setup_routes(app):
     app.router.add_route('GET', '/v1/pub', IframesView)
     app.router.add_route('POST', '/v1/pub', IframesView)
     app.router.add_route('POST', '/v1/advertises.json', AdvertisesView)
-    app.router.add_static('/v1/static/', app['config']['dir_path'] + '/static/', append_version=True)
+    # app.router.add_static('/v1/static/', app['config']['dir_path'] + '/static/', append_version=True)
