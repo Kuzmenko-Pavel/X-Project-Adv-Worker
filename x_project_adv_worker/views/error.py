cd4d937346284ff5ce78ab47abc17cbee473fff2
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-09-19 17:05:42 +0300

    change error log

--- x_project_adv_worker/views/error.py
+++ x_project_adv_worker/views/error.py
@@ -16,9 +16,10 @@ class ErrorView(web.View):
         data = {}
         try:
             data = await self.request.json(loads=ujson.loads)
-            logger.warning(
-                'ERROR AJAX REQUEST %s %s %s ' % (data.get('url', ''), data.get('data', {}).get('block_id', ''),
-                                                  data.get('statusText', '')))
+            pass
+            # logger.warning(
+            #     'ERROR AJAX REQUEST %s %s %s ' % (data.get('url', ''), data.get('data', {}).get('block_id', ''),
+            #                                       data.get('statusText', '')))
         except Exception as ex:
             logger.error(exception_message(exc=str(ex), request=str(self.request.message), data=data))
         return web.json_response(result, dumps=ujson.dumps)
