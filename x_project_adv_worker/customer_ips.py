8a42b2f009c03ed18a2e6062793faacc5cce2d79
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-03-03 13:12:44 +0200

    change user agent

--- x_project_adv_worker/customer_ips.py
+++ x_project_adv_worker/customer_ips.py
@@ -9,11 +9,16 @@ with open(json_path) as json_file:
     data = json.load(json_file)
     for item in data:
         res.append(item)
-
 # PROM.UA
 res.append('193.34.169')
 # META.UA
 res.append('194.0.131')
+# LOCALHOST
+res.append('127.0.0')
+# EMPTY
+res.append('0.0.0')
+# OFFICE
+res.append('178.165.81.178')
 
 res = sorted(res, key=lambda item: socket.inet_aton(item))
 ip_pattern = re.compile('|'.join(x.replace('.', '\.') for x in res))
